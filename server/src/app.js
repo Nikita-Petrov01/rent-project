bd6f6b08c91c05e01f437d60627f248dfe949cb5
Nikita-Petrov0 <petrovn.46.22@gmail.com>
2025-04-04 17:13:55 +0300

    deploy

--- server/src/app.js
+++ server/src/app.js
@@ -32,7 +32,7 @@ app.use('/api/giga', gigaRouter);
 
 app.use('/api/likes', likeRouter);
 
-app.use(express.static.static(path.join(__dirname, '..', 'dist')));
+app.use(express.static(path.join(__dirname, '..', 'dist')));
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
 })
